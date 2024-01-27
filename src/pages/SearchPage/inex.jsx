ed5b8afd1550078c2d6c67caacb96ae79037f567
michelmnds <m1tchofc@gmail.com>
2024-01-27 00:14:03 +0000

    feat: search logic done

--- src/pages/SearchPage/inex.jsx
+++ src/pages/SearchPage/inex.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-undef */
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./style.css";
 import { PersonalTrainerContext } from "../../providers/PersonalTrainerContext";
 
@@ -10,50 +11,98 @@ export const SearchPage = () => {
 
   const finalObj = { city, speciality, gender };
 
-  const { ptList } = useContext(PersonalTrainerContext);
+  const {
+    ptList,
+    getAllPts,
+    cityList,
+    getAvaliableCities,
+    specialityList,
+    getAllSpecialities,
+  } = useContext(PersonalTrainerContext);
+
+  useEffect(() => {
+    getAvaliableCities();
+    getAllSpecialities();
+    getAllPts();
+    console.log(cityList);
+  }, []);
+
+  const findPt = () => {
+    const fetchedPtList = ptList.filter(
+      (pt) =>
+        pt.location === finalObj.city &&
+        pt.gender === finalObj.gender &&
+        pt.speciality.includes(finalObj.speciality)
+    );
+    console.log(fetchedPtList);
+    return fetchedPtList;
+  };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    findPt();
     console.log(finalObj);
   };
 
-  return (
-    <div className="searchPageContainer">
-      <form className="searchForm" onSubmit={handleSubmit}>
-        <label htmlFor="cidade">
-          Cidade:
-          <select
-            id="cidade"
-            type="text"
-            value={city}
-            onChange={(event) => setCity(event.target.value)}
-          >
-            <option value="null">Selecione uma cidade</option>
-          </select>
-        </label>
+  if (cityList.length !== 0) {
+    return (
+      <div className="searchPageContainer">
+        <form className="searchForm" onSubmit={handleSubmit}>
+          <label htmlFor="cidade">
+            Cidade:
+            <select
+              id="cidade"
+              type="text"
+              value={city}
+              onChange={(event) => setCity(event.target.value)}
+            >
+              <option value="null">Selecione uma cidade</option>
+              {cityList.map((city, index) => {
+                return (
+                  <option key={index} value={city.name}>
+                    {city.name}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
 
-        <label htmlFor="especialidade">
-          Especialidade:
-          <input
-            id="especialidade"
-            type="text"
-            value={speciality}
-            onChange={(event) => setSpeciality(event.target.value)}
-          />
-        </label>
+          <label htmlFor="especialidade">
+            Especialidade:
+            <select
+              id="especialidade"
+              type="text"
+              value={speciality}
+              onChange={(event) => setSpeciality(event.target.value)}
+            >
+              <option value="null">Selecione uma especialidade</option>
+              {specialityList.map((speciality, index) => {
+                return (
+                  <option key={index} value={speciality.name}>
+                    {speciality.name}
+                  </option>
+                );
+              })}
+            </select>
+          </label>
 
-        <label htmlFor="genero">
-          Gênero:
-          <input
-            id="genero"
-            type="text"
-            value={gender}
-            onChange={(event) => setGender(event.target.value)}
-          />
-        </label>
+          <label htmlFor="genero">
+            Gênero:
+            <select
+              id="genero"
+              type="text"
+              value={gender}
+              onChange={(event) => setGender(event.target.value)}
+            >
+              <option value="null">Selecione o gênero</option>
+              <option value="Female">Feminino</option>
+              <option value="Male">Masculino</option>
+            </select>
+          </label>
 
-        <button className="formBtn">BUSCAR</button>
-      </form>
-    </div>
-  );
+          <button className="formBtn">BUSCAR</button>
+        </form>
+      </div>
+    );
+  }
 };
--- src/providers/PersonalTrainerContext.jsx
+++ src/providers/PersonalTrainerContext.jsx
@@ -9,6 +9,8 @@ const API_URL = import.meta.env.VITE_API_URL;
 export const PersonalTrainerContextProvider = ({ children }) => {
   const [ptList, setPtLits] = useState([]);
   const [singlePt, setSinglePt] = useState({});
+  const [cityList, setCityLits] = useState([]);
+  const [specialityList, setSpecialityLits] = useState([]);
 
   const getAllPts = async () => {
     try {
@@ -21,9 +23,42 @@ export const PersonalTrainerContextProvider = ({ children }) => {
     }
   };
 
+  const getAllSpecialities = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/specialities`);
+      if (response.status === 200) {
+        setSpecialityLits(response.data);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getAvaliableCities = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/cities`);
+
+      setCityLits(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <PersonalTrainerContext.Provider
-      value={{ getAllPts, ptList, singlePt, setSinglePt, setPtLits }}
+      value={{
+        getAllPts,
+        getAvaliableCities,
+        getAllSpecialities,
+        cityList,
+        specialityList,
+        ptList,
+        singlePt,
+        setCityLits,
+        setSpecialityLits,
+        setSinglePt,
+        setPtLits,
+      }}
     >
       {children}
     </PersonalTrainerContext.Provider>
